d42e651bff485876f10828892ccd2ac2648a3a25
Avery Oliver <averyaoliver03@gmail.com>
2024-04-22 22:02:34 -0500

    modified

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'myapp/index.html', {'products': products})
+
+def show(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    return render(request, 'myapp/show.html', {'product': product})
