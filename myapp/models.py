95fdc2417f46176d3904827d3dba74b130ac9adc
Avery Oliver <averyaoliver03@gmail.com>
2024-04-29 23:12:18 -0500

    updated

--- myapp/models.py
+++ myapp/models.py
@@ -2,9 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
 
-    def __str__(self):
-        return self.name
 
--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,7 @@ def index(request):
     products = Product.objects.all()
     return render(request, 'myapp/index.html', {'products': products})
 
-def show(request, product_id):
-    product = Product.objects.get(pk=product_id)
+def show(request, id):
+    product = Product.objects.get(id=id)
     return render(request, 'myapp/show.html', {'product': product})
+
